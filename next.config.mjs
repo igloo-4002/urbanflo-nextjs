563e9a9c46005db94b61092c65b455900aa61e51
Dylan Clements <dylan.clements99@gmail.com>
2023-07-31 10:40:58 +1000

    remove call to transpile monorepo packages

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,6 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  /** Enables hot reloading for local packages without a build step */
-  transpilePackages: ["@igloo/api", "@igloo/db"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   typescript: { ignoreBuildErrors: !!process.env.CI },
